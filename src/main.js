1bfdb1c1e8a5a877990e5b372be57e51a3eff7e0
jjori <jjori.master@gmail.com>
2019-07-09 21:39:29 +0900

    프로젝트 초기설정

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import App from './App';
+
+Vue.use(Vuetify, {
+  iconfont: 'fa',
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
