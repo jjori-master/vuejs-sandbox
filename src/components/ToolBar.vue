9675b47d681560bcab2deee6bec10018c4a59663
jjori <jjori.master@gmail.com>
2019-07-16 02:08:16 +0900

    이렇게 까지 해야 되나~~~ㅠㅠ

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div class="header">
     <router-link to="/news">News</router-link>
+    <span class="line">|</span>
     <router-link to="/ask">Ask</router-link>
+    <span class="line">|</span>
     <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
@@ -10,4 +12,17 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+
+.header span.line {
+  display: inline-block;
+  margin: 0 3px;
+  color: #02251a;
+}
+</style>
