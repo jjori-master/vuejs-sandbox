4204a580f00a3cd8a852b18a15d7fb864555641f
jjori <jjori.master@gmail.com>
2019-07-15 22:44:21 +0900

    header 컴포넌트 생성 및 각 링크 router-link로 연결

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {};
+import ToolBar from './components/ToolBar.vue';
+export default {
+  components: {
+    ToolBar,
+  },
+};
 </script>
 
 <style></style>
